bba6d2fd33bf24ad24b8d0bb41bf04ce5799210e
naisannovel <naisannovel@gmail.com>
2021-02-02 21:21:13 +0600

    javascript updated

--- funtional-javascript/Return_a_Function_from_Another_Function.js
+++ funtional-javascript/Return_a_Function_from_Another_Function.js
@@ -1,14 +1,28 @@
 
 function greet(message){
+
+    // create inner function.
+
     function greetings(name){
         return `${message} ${name}`;
     }
+
+    // return inner function.
+
     return greetings;
 }
 
 const callGreetFunc = greet('good morning');
-// callGreetFunc also a function.
+// callGreetFunc also a function. because we return inner function
+// for that callGreetFunc hold inner function.
 
 const greetingFunc = callGreetFunc('naisan');
 
 console.log(greetingFunc);
+
+
+
+
+
+
+
