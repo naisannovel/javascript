875e09fc29d095a2ddac8f1bfc51adf68d616de0
naisannovel <naisannovel@gmail.com>
2021-02-09 01:17:50 +0600

    update

---
+++ functional-javascript/currying.js
@@ -0,0 +1,19 @@
+
+function sum(a,b){
+    return a + b;
+}
+
+function sub(num){
+    return num - 2;
+}
+
+function multiply(num){
+    return num * num;
+}
+
+let result = multiply(sub(sum(2,3)));
+console.log(result);    // 9
+
+// akta function ar input aakare onno akta function ar value asbe.
+// abar onno function tar input aakare onno arekta function ar value asbe. 
+// ata ke currying bole.
