7c64b272c5189330974f5e343388207c10a25b74
naisannovel <naisannovel@gmail.com>
2021-02-11 03:09:22 +0600

    update

--- OOP/addPrototype.js
+++ OOP/addPrototype.js
@@ -15,3 +15,27 @@ let obj2 = {}
 Object.prototype.something = 2546545;
 // console.log(obj2);  
 // obj2 ar parent ai something prototype ta hold kore ace.
+
+// --------------------instance--------------------------------
+
+// # instance member hocce constructor ar nijer property.
+// # prototype member hocce constructor ar proto/parent ar property.
+// # instance constructor prototype ar property te access pabe.
+// # constructor prototype instance ar property te access pabe.
+
+function Draw(name){
+    this.name = name
+
+    // this.rectangle = function(){
+    //     console.log('this is rectangle method');
+    // }
+}
+
+Draw.prototype = {
+    rectangle: function(){
+        console.log('this is rectangle method');
+    }
+}
+
+let draw1 = new Draw('naisan')
+console.log(draw1);
