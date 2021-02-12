7da15cd8a78e33b35cbd812ee35168bde94cb70b
naisannovel <naisannovel@gmail.com>
2021-02-12 17:56:08 +0600

    update

---
+++ OOP/inheritance.js
@@ -0,0 +1,55 @@
+function Common(color){
+    this.color = color
+}
+
+Common.prototype.common = function(){
+    console.log('this is common method');
+}
+
+function Shape(width,height,color){
+    this.width = width
+    this.height = height
+    Common.call(this,color)     // this is super call.
+}
+
+Shape.prototype = Object.create(Common.prototype)   // inheritance
+Shape.prototype.constructor = Shape                 // reset constructor
+
+// Shape.prototype.common = function(){                 // common method overwrite
+//     Common.prototype.common.call(this)      // parent common method call
+//     console.log('this overWrite common method');
+// }
+
+Shape.prototype.draw = function(){
+    console.log('this is draw method');
+}
+
+let shape1 = new Shape(10,15,'green')
+console.log(shape1);
+
+
+
+
+
+// -----------------Object.assign method and composition--------------------------------
+
+let canWalk = {
+    walk: function(){
+        console.log('walking...');
+    }
+}
+
+let canEat = {
+    eat: function(){
+        console.log('eating...');
+    }
+}
+
+// let person = Object.assign({},canWalk,canEat)          // return new person obj 
+
+function Person(name){
+    this.name = name
+}
+Object.assign(Person.prototype,canEat,canWalk)
+
+let person1 = new Person('naisan')
--- OOP/try.js
+++ OOP/try.js
@@ -159,15 +159,116 @@
 // let des = Object.getOwnPropertyDescriptor(pro,'propertyIsEnumerable')
 // console.log(des);
 
-function Circle(name){
-    this.name = name
+// function Circle(name){
+//     this.name = name
+// }
+
+// Circle.prototype.pi = 25412
+// let name1 = new Circle('naisan')
+// let name2 = new Circle('naisan')
+// let name3 = new Circle('naisan')
+
+// console.log(name1);
+// console.log(name2);
+// console.log(name3);
+
+
+// function Circle(width,height){
+//     this.width = width
+//     this.height = height
+// }
+// Circle.prototype = {
+//     rectangle: function(){
+//         console.log('this is rectangle method');
+//     },
+//     draw: function(){
+//         console.log('this is draw method');
+//     }
+// }
+
+// let cir1 = new Circle(25,45)
+// Object.defineProperty(cir1,'width',{
+//     writable: false,
+//     enumerable: false,
+//     configurable: false
+// })
+
+// console.log(cir1);
+// console.log(Object.getOwnPropertyDescriptor(cir1,'width'));
+// console.log(Object.getOwnPropertyDescriptor(cir1,'height'));
+// console.log(cir1.hasOwnProperty('width'));
+// console.log(Object.getPrototypeOf(cir1.rectangle));
+// console.log(Object.getPrototypeOf(cir1));
+
+// let x = Object.getPrototypeOf(cir1)
+// let y = Object.getPrototypeOf(x)
+// console.log(y);
+
+// function Shape(){
+
+// }
+// Shape.prototype = {
+//     draw :function(){
+//         console.log('this is draw method');
+//         this.circle()
+//     }
+// }
+
+// function Rectangle(width,height){
+//     this.width = width
+//     this.height = height
+
+//     this.circle = function(){
+//         console.log('this is circle method');
+//         // this.draw()
+//         console.log(this.width,this.height);
+//     }
+// }
+
+// Rectangle.prototype = Object.assign(Shape.prototype)
+// // Rectangle.prototype = Object.create(Shape.prototype)
+// Rectangle.prototype.common = function(){
+//     console.log('this common method');
+// }
+
+
+// let rect = new Rectangle(45,69)
+
+// // console.log(Rectangle.prototype);
+
+
+// let obj1 = {
+//     name: 'naisan',
+//     roll: 25
+// }
+// console.log(obj1);
+// let obj2 = Object.assign(obj1)
+// console.log(obj2);
+// console.log(Object.getPrototypeOf(obj2));
+
+function Common(color){
+    this.color = color
+}
+Common.prototype = {
+    common: function(){
+        console.log('this is common method');
+    }
+}
+let com = new Common('green')
+
+function Draw(width,height,color){
+    this.width = width
+    this.height = height  
 }
 
-Circle.prototype.pi = 25412
-let name1 = new Circle('naisan')
-let name2 = new Circle('naisan')
-let name3 = new Circle('naisan')
+Draw.prototype = Object.create(Common.prototype);
+Draw.prototype.constructor = Draw;
 
-console.log(name1);
-console.log(name2);
-console.log(name3);
+Draw.prototype.common = function(){
+    com.common.call(this)
+    console.log('this is common method 2');
+}
+
+
+let draw1 = new Draw(15,25)
+console.log(draw1);
